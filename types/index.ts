63ced56d50965e10cf9487af1b78fc00568da709
Adrian Albert Ciubucciu <aciubucciu@deloittece.com>
2023-10-21 18:16:58 +0300

    fetch cars with filters from url

--- types/index.ts
+++ types/index.ts
@@ -34,3 +34,15 @@ export interface ICar {
     transmission: string;
     year: number;
 }
+
+export interface FilterProps {
+    manufacturer?: string;
+    year?: number;
+    model?: string;
+    limit?: number;
+    fuel?: string;
+  }
+  
+export interface HomeProps {
+    searchParams: FilterProps;
+  }
--- utils/index.ts
+++ utils/index.ts
@@ -1,15 +1,21 @@
-import { ICar } from "@/types";
+import { FilterProps, ICar } from "@/types";
 
-export async function fetchCars() {
+export async function fetchCars(filters: FilterProps) {
+    const { manufacturer, year, model, limit, fuel } = filters;
+
+    // Set the required headers for the API request
     const headers = {
         'X-RapidAPI-Key': '37fb707816msh0359da1a1346975p1dbaa0jsn1ae674d78fc3',
         'X-RapidAPI-Host': 'cars-by-api-ninjas.p.rapidapi.com'
     }
 
-    const response = await fetch('https://cars-by-api-ninjas.p.rapidapi.com/v1/cars?model=x6', {
+    // Set the required headers for the API request
+    const response = await fetch(
+        `https://cars-by-api-ninjas.p.rapidapi.com/v1/cars?make=${manufacturer}&year=${year}&model=${model}&limit=${limit}&fuel_type=${fuel}`, {
         headers: headers,
     });
 
+    // Parse the response as JSON
     const result = await response.json();
 
     return result;
